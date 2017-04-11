09d9cf29a8f7e87657233721ee9cb061f1ffd307
Joy Patel <joypatel0123@gmail.com>
2017-04-11 18:07:39 -0400

    Updated MusicSpeech.'

--- MusicSpeech.py
+++ MusicSpeech.py
@@ -18,6 +18,23 @@ from SlidingWindow import *
 from MusicFeatures import *
 import scipy.io.wavfile
 
+def getRips(filename):
+    FsMusic, XMusic = scipy.io.wavfile.read(filename)
+    hopSize = 50
+    novFnMusic = getAudioNovelty(XMusic, FsMusic, hopSize)
+    dim = 20
+    Tau = (FsMusic/2)/(float(hopSize)*dim)
+    dT = 1
+    Y = getSlidingWindowInteger(novFnMusic, dim, Tau, dT)
+    Y = Y - np.mean(Y, 1)[:, None]
+    Y = Y/np.sqrt(np.sum(Y**2, 1))[:, None]
+
+    PDs = doRipsFiltration(Y, 1)
+    d0 = PDs[0]
+    d1 = PDs[1]
+    #return d0[:,1] - d0[:,0], d1[:,1] - d1[:,0]
+    delta = np.array(sorted(d0[:,1] - d0[:,0], reverse=True))[0:30]
+    return delta
 
 def binPD (PD, birth_max, death_max, birth_min=0.0, death_min=0.0, axis_bins=3):
 
@@ -33,11 +50,11 @@ def binPD (PD, birth_max, death_max, birth_min=0.0, death_min=0.0, axis_bins=3):
 
 if __name__ == '__main__':
     #Don't Stop Believing
-    FsMusic, XMusic = scipy.io.wavfile.read("Austin/2017_03_30_ao_2.wav") 
+    FsMusic, XMusic = scipy.io.wavfile.read("Austin/2017_03_30_ao_2.wav")
     FsSpeech, XSpeech = scipy.io.wavfile.read("2017_03_30_jp.wav")
-    
+
     #Step 1: Try a raw delay embedding
-    #Note that dim*Tau here spans a half a second of audio, 
+    #Note that dim*Tau here spans a half a second of audio,
     #since Fs is the sample rate
     #dim = round(FsMusic/200)
     #Tau = 100
@@ -60,12 +77,12 @@ if __name__ == '__main__':
     #plt.subplot(122)
     #plotDGM(PDs[1])
     #plt.title("Persistence Diagram")
-    
-    
+
+
     #Step 2: Do sliding window on audio novelty functions
     #(sliding window of sliding windows!)
     hopSize = 512
-    
+
     #First do audio novelty function on music
     novFnMusic = getAudioNovelty(XMusic, FsMusic, hopSize)
     (MFCC, MFCCDCT, P) = getMFCC(XMusic, FsMusic, hopSize)
@@ -98,7 +115,7 @@ if __name__ == '__main__':
     plotDGM(PDs[1])
     plt.title("Persistence Diagram")
 
-    
+
     ##Now do audio novelty function on speech
     #novFnSpeech = getAudioNovelty(XSpeech, FsSpeech, hopSize)
     #dim = 20
@@ -124,4 +141,3 @@ if __name__ == '__main__':
     ##plotDGM(PDs[1])
     ##plt.title("Persistence Diagram")
     plt.show()
-   
