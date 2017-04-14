e6a129148ba96244ef7f281f59c66765823a3a02
Joy Patel <joypatel0123@gmail.com>
2017-04-14 15:41:56 -0400

    Added cross correlation and multiscale kernel.

--- DistanceFunctions.py
+++ DistanceFunctions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 '''
 Returns pairwise distance matrix between points clouds pc0 and pc1.
@@ -18,6 +19,25 @@ Finds Euclidean distance between vectors u and v.
 def euclidean(u, v):
     return np.linalg.norm(u - v)
 
+'''
+Finds the cross correlation between raw inputs u and v.
+'''
+def cross_correlation(u, v):
+    return np.correlate(u,v)
+
+'''
+Multiscale kernel distance between 2 persistence diagrams P and Q
+'''
+def multiscale_kernel(P, Q):
+    kernel = 0.0
+    norm = lambda x, y : math.pow(math.pow(x[0]-y[0],2) + math.pow(x[1]-y[1],2),0.5)
+    for p in P:
+        for q in Q:
+            kernel += math.pow(math.e,-1.0*math.pow(norm(p,q),2)/(8.0*sigma)) - math.pow(math.e,-1.0*math.pow(norm(p,np.array([q[1],q[0]])),2)/(8.0*sigma))
+    return kernel/(8.0*math.pi*sigma)
+
 dist_functions = {
-    "euclidean" : euclidean
+    "euclidean" : euclidean,
+    "cross_correlation" : cross_correlation,
+    "multiscale_kernel" : multiscale_kernel
 }
